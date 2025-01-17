196f60489f3f5b34690cba30d1ec74137ca2d6bf
Damla Kemik <damla.s.k05@gmail.com>
2025-01-17 23:57:17 +0200

    initial commit

---
+++ src/main/java/example/ReadingFromConsole.java
@@ -0,0 +1,29 @@
+package example;
+
+import java.util.Scanner;
+
+public class ReadingFromConsole {
+
+    public static void main(String[] args) {
+        //Print the text in the console
+        System.out.println("Please fill in name:");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("Nice to meet you, " + userName + "!");
+
+        System.out.println("Please enter your age:");
+
+        int userAge = scanner.nextInt();
+
+        System.out.println("What a wonderful age you have, " + userAge + "!");
+
+        System.out.println("How many hobbies do you have:");
+
+        int userHobby = scanner.nextInt();
+
+        System.out.println("Excellent, " + userName + "!");
+
+    }
+}
